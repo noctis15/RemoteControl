bbc0021028f983747ca1450e50ffed444f54f5b0
noctis15 <maciej.lickiewicz1@gmail.com>
2015-11-02 16:19:26 +0100

    WifiDirect  + File transfer

--- app/src/main/java/com/remote/noctis/remotecontrol/MainActivity.java
+++ app/src/main/java/com/remote/noctis/remotecontrol/MainActivity.java
@@ -1,253 +1,165 @@
 package com.remote.noctis.remotecontrol;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.p2p.WifiP2pConfig;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements WifiP2pManager.ChannelListener, DeviceListFragment.DeviceActionListener {
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-    private final IntentFilter intentFilter = new IntentFilter();
+public class MainActivity extends Activity  {
+    //client side
+    private Socket client;
+    private FileInputStream fileInputStream;
+    private BufferedInputStream bufferedInputStream;
+    private OutputStream outputStream;
+    private Button button;
+    private TextView text;
 
-    private Channel wifiChannel;
-    private WifiP2pManager wifiManager;
-    private boolean isWifiEnabled = false;
-    private BroadcastReceiver receiver = null;
-    private boolean retryChannel = false;
-    public static final String TAG = "WiFiMainActivity";
+    //server side
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static InputStreamReader inputStreamReader;
+    private static BufferedReader bufferedReader;
+    private static String message;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        button = (Button) findViewById(R.id.button1);   //reference to the send button
+        text = (TextView) findViewById(R.id.textView1);   //reference to the text view
+
+
+        //Button press event listener
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        try {
+                            Socket client = new Socket("192.168.0.12", 8187);
+                            DataOutputStream DOS = new DataOutputStream(client.getOutputStream());
+                            DOS.writeUTF("COSIK!!!!!!!!!!!!!");
+                            client.close();
+                        } catch (UnknownHostException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
             }
         });
 
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(8187); // Server socket
 
-        //  Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-
-        // Indicates a change in the list of available peers.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-
-        // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-
-        // Indicates this device's details have changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        wifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        wifiChannel = wifiManager.initialize(this, getMainLooper(), null);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        receiver = new WifiBroadcastReceiver(wifiManager, wifiChannel, this);
-        registerReceiver(receiver, intentFilter);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
-    /**
-     * Remove all peers and clear all fields. This is called on
-     * BroadcastReceiver receiving a state change event.
-     */
-    public void resetData() {
-        DeviceListFragment fragmentList = (DeviceListFragment) getFragmentManager()
-                .findFragmentById(R.id.frag_list);
-        DeviceDetailFragment fragmentDetails = (DeviceDetailFragment) getFragmentManager()
-                .findFragmentById(R.id.frag_detail);
-        if (fragmentList != null) {
-            fragmentList.clearPeers();
-        }
-        if (fragmentDetails != null) {
-            fragmentDetails.resetViews();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.atn_direct_enable:
-                if (wifiManager != null && wifiChannel != null) {
-
-                    // Since this is the system wireless settings activity, it's
-                    // not going to send us a result. We will be notified by
-                    // WiFiDeviceBroadcastReceiver instead.
-
-                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
-                } else {
-                    Log.e(TAG, "channel or manager is null");
+                } catch (IOException e) {
+                    System.out.println("Could not listen on port: 8187");
                 }
-                return true;
 
-            case R.id.atn_direct_discover:
-                if (!isWifiEnabled) {
-                    Toast.makeText(MainActivity.this, R.string.p2p_off_warning,
-                            Toast.LENGTH_SHORT).show();
-                    return true;
+                System.out.println("Server started. Listening to the port 88187");
+
+                while (true) {
+                    try {
+
+                        clientSocket = serverSocket.accept(); // accept the client
+                        // connection
+                        inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+                        bufferedReader = new BufferedReader(inputStreamReader); // get
+                        // client
+                        // msg
+                        message = bufferedReader.readLine();
+
+                        System.out.println(message);
+
+                        showToast(message);
+                        //Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+                        inputStreamReader.close();
+                        clientSocket.close();
+
+                    } catch (IOException ex) {
+                        System.out.println("Problem in message reading");
+                    }
                 }
-                final DeviceListFragment fragment = (DeviceListFragment) getFragmentManager()
-                        .findFragmentById(R.id.frag_list);
-                fragment.onInitiateDiscovery();
-                wifiManager.discoverPeers(wifiChannel, new WifiP2pManager.ActionListener() {
 
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(MainActivity.this, "Discovery Initiated",
-                                Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFailure(int reasonCode) {
-                        Toast.makeText(MainActivity.this, "Discovery Failed : " + reasonCode,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void showDetails(WifiP2pDevice device) {
-        DeviceDetailFragment fragment = (DeviceDetailFragment) getFragmentManager()
-                .findFragmentById(R.id.frag_detail);
-        fragment.showDetails(device);
-
-    }
-
-    @Override
-    public void connect(WifiP2pConfig config) {
-        wifiManager.connect(wifiChannel, config, new WifiP2pManager.ActionListener() {
-
-            @Override
-            public void onSuccess() {
-                // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
             }
+        };
 
-            @Override
-            public void onFailure(int reason) {
-                Toast.makeText(MainActivity.this, "Connect failed. Retry.",
-                        Toast.LENGTH_SHORT).show();
+        thread.start();
+
+
+
+    }
+
+    public void showToast(final String toast)
+    {
+        runOnUiThread(new Runnable() {
+            public void run()
+            {
+                Toast.makeText(MainActivity.this, toast, Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    @Override
-    public void disconnect() {
-        final DeviceDetailFragment fragment = (DeviceDetailFragment) getFragmentManager()
-                .findFragmentById(R.id.frag_detail);
-        fragment.resetViews();
-        wifiManager.removeGroup(wifiChannel, new WifiP2pManager.ActionListener() {
 
-            @Override
-            public void onFailure(int reasonCode) {
-                Log.d(TAG, "Disconnect failed. Reason :" + reasonCode);
-            }
+    public class ExcuteNetworkOperation extends AsyncTask<Void, Void, String> {
 
-            @Override
-            public void onSuccess() {
-                fragment.getView().setVisibility(View.GONE);
-            }
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            /**
+             * show dialog
+             */
+            super.onPreExecute();
+        }
 
-        });
-    }
+        @Override
+        protected String doInBackground(Void... params) {
+            // TODO Auto-generated method stub
+            /**
+             * Do network related stuff
+             * return string response.
+             */
+            return null;
+        }
 
-    @Override
-    public void onChannelDisconnected() {
-        // we will try once more
-        if (wifiManager != null && !retryChannel) {
-            Toast.makeText(this, "Channel lost. Trying again", Toast.LENGTH_LONG).show();
-            resetData();
-            retryChannel = true;
-            wifiManager.initialize(this, getMainLooper(), this);
-        } else {
-            Toast.makeText(this,
-                    "Severe! Channel is probably lost premanently. Try Disable/Re-Enable P2P.",
-                    Toast.LENGTH_LONG).show();
+        @Override
+        protected void onPostExecute(String result) {
+            // TODO Auto-generated method stub
+            /**
+             * update ui thread and remove dialog
+             */
+            super.onPostExecute(result);
         }
     }
 
-    @Override
-    public void cancelDisconnect() {
 
-        /*
-         * A cancel abort request by user. Disconnect i.e. removeGroup if
-         * already connected. Else, request WifiP2pManager to abort the ongoing
-         * request
-         */
-        if (wifiManager != null) {
-            final DeviceListFragment fragment = (DeviceListFragment) getFragmentManager()
-                    .findFragmentById(R.id.frag_list);
-            if (fragment.getDevice() == null
-                    || fragment.getDevice().status == WifiP2pDevice.CONNECTED) {
-                disconnect();
-            } else if (fragment.getDevice().status == WifiP2pDevice.AVAILABLE
-                    || fragment.getDevice().status == WifiP2pDevice.INVITED) {
-
-                wifiManager.cancelConnect(wifiChannel, new WifiP2pManager.ActionListener() {
-
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(MainActivity.this, "Aborting connection",
-                                Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onFailure(int reasonCode) {
-                        Toast.makeText(MainActivity.this,
-                                "Connect abort request failed. Reason Code: " + reasonCode,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        }
-
-    }
-
-    public boolean isWifiEnabled() {
-        return isWifiEnabled;
-    }
-
-    public void setIsWifiEnabled(boolean isWifiEnabled) {
-        this.isWifiEnabled = isWifiEnabled;
-    }
 }
